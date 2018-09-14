878a14c3a4804c6e013382e37833f53cfff3d82f
Tom Logan <tomlogan@umich.edu>
2018-09-14 11:44:45 -0400

    clipping state building footprint to city

--- code/processing/footprint.R
+++ code/processing/footprint.R
@@ -1,22 +1,28 @@
 library(rgdal)
 library(raster)
-
+library(rgeos)
 
 # import geojson
-state <- 'Arizona'
-city.name <- 'phx'
-file.name <- paste0('B:/research/land_surface_temperature/data/building_footprint/',state,'/',state,'.geojson')
+state <- 'Michigan'
+city.name <- 'det'
+# file.name <- paste0('B:/research/land_surface_temperature/data/building_footprint/',state,'/',state,'.geojson')
+file.name <- paste0('/mnt/StorageArray/tlogan/research/urban_data/land_surface_temperature/data/building_footprint/',state,'/',state,'.geojson')
 df <- readOGR(dsn=file.name,layer = 'OGRGeoJSON')
 
+# address invalid geometry issue
+df <- gBuffer(df, width=0, byid=T)
+
 # import city polygon
-city.dir <- paste0('B:/research/land_surface_temperature/data/', city.name)
-city <- readOGR(dsn = city.dir, layer = paste0(city,'_boundary'), verbose = FALSE)
+# city.dir <- paste0('B:/research/land_surface_temperature/data/', city.name)
+city.dir <- paste0('/mnt/StorageArray/tlogan/research/urban_data/land_surface_temperature/data/', city.name)
+city <- readOGR(dsn = city.dir, layer = paste0(city.name,'_boundary'), verbose = FALSE)
 
 # clip the polygons to the city
 bldg.city <- intersect(df,city)
-
+df <- NULL # clear memory
 
 # export shapefile
-exp.dsn <- paste0('B:/research/land_surface_temperature/data/building_footprint/',state)
+# exp.dsn <- paste0('B:/research/land_surface_temperature/data/building_footprint/',state)
+exp.dsn <- paste0('/mnt/StorageArray/tlogan/research/urban_data/land_surface_temperature/data/building_footprint/',state)
 exp.layer <- paste0('building_footprint_',city.name)
 writeOGR(obj=bldg.city, dsn=exp.dsn, layer=exp.layer, driver="ESRI Shapefile", overwrite_layer = T)
